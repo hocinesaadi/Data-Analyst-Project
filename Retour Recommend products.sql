16001445cb8b07968af9d6b9763b323c9552ecd8
saadi <34628341+hocinesaadi@users.noreply.github.com>
2022-02-01 15:46:57 +0100

    Create Retour Recommend products.sql

---
+++ Retour Recommend products.sql
@@ -0,0 +1,104 @@
+/*
+Recommend products with Cloud SQL and Spark
+Before you start working in Google Cloud :
+1-You need to make sure you have the correct permissions for your project in IAM (Identity and Access Management).
+2-Create a Cloud SQL instance.
+3-Organize data in Cloud Storage.
+4-Load data from Cloud Storage into Cloud SQL tables
+5-Run Dataproc :train a model of recommendations based on machine learning (previous user ratings) using Dataproc.
+
+
+
+
+
+
+*/
+/*Create tables */
+CREATE DATABASE IF NOT EXISTS recommendation_spark;
+USE recommendation_spark;
+DROP TABLE IF EXISTS Recommendation;
+DROP TABLE IF EXISTS Rating;
+DROP TABLE IF EXISTS Accommodation;
+CREATE TABLE IF NOT EXISTS Accommodation
+(
+  id varchar(255),
+  title varchar(255),
+  location varchar(255),
+  price int,
+  rooms int,
+  rating float,
+  type varchar(255),
+  PRIMARY KEY (ID)
+);
+CREATE TABLE  IF NOT EXISTS Rating
+(
+  userId varchar(255),
+  accoId varchar(255),
+  rating int,
+  PRIMARY KEY(accoId, userId),
+  FOREIGN KEY (accoId)
+    REFERENCES Accommodation(id)
+);
+CREATE TABLE  IF NOT EXISTS Recommendation
+(
+  userId varchar(255),
+  accoId varchar(255),
+  prediction float,
+  PRIMARY KEY(userId, accoId),
+  FOREIGN KEY (accoId)
+    REFERENCES Accommodation(id)
+);
+SHOW DATABASES;
+
+CREATE DATABASE IF NOT EXISTS recommendation_spark;
+USE recommendation_spark;
+DROP TABLE IF EXISTS Recommendation;
+DROP TABLE IF EXISTS Rating;
+DROP TABLE IF EXISTS Accommodation;
+CREATE TABLE IF NOT EXISTS Accommodation
+(
+  id varchar(255),
+  title varchar(255),
+  location varchar(255),
+  price int,
+  rooms int,
+  rating float,
+  type varchar(255),
+  PRIMARY KEY (ID)
+);
+CREATE TABLE  IF NOT EXISTS Rating
+(
+  userId varchar(255),
+  accoId varchar(255),
+  rating int,
+  PRIMARY KEY(accoId, userId),
+  FOREIGN KEY (accoId)
+    REFERENCES Accommodation(id)
+);
+CREATE TABLE  IF NOT EXISTS Recommendation
+(
+  userId varchar(255),
+  accoId varchar(255),
+  prediction float,
+  PRIMARY KEY(userId, accoId),
+  FOREIGN KEY (accoId)
+    REFERENCES Accommodation(id)
+);
+SHOW DATABASES;
+/* apply the model from Dataproc to create a list of recommendations for each user in the database.*/
+/*Explore inserted rows with SQL*/
+
+SELECT
+    r.userid,
+    r.accoid,
+    r.prediction,
+    a.title,
+    a.location,
+    a.price,
+    a.rooms,
+    a.rating,
+    a.type
+FROM Recommendation as r
+JOIN Accommodation as a
+ON r.accoid = a.id
+WHERE r.userid = 10;
